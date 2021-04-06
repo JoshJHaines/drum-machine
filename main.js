aa5ef53f59a13f238c97b04e88f0ce5f16ea81a9
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-06 11:26:59 -0400

    added tock sound, have a count that increases with each tick. Not playing tock and not reseting count

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3');
+
+let count = 0
 
 // This function is called every 600ms
 function update() {
 
     // Play the 'tick' sound
     tick.play();
+    count++;
+    console.log(count)
+    if(count === 4){
+    tock.play();
+    }
 }
 
 // This function sets up update() to be called every 600ms
