32c43864e1ba69843134e4e6b00f72c32c0bed1a
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-06 14:01:18 -0400

    2b complete. adjusted the metronome to TOCK on 3 count because the order that the code is running. Visusally it tocks on 4.

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ const boxKickDrum = document.querySelector("#kick-drum")
 const boxSnareDrum = document.querySelector("#snare-drum")
 const boxHiHat = document.querySelector("#hi-hat")
 
-boxMetro.checked = true
+boxMetro.checked = false
 boxKickDrum.checked = false
 boxSnareDrum.checked = false
 boxHiHat.checked = false
@@ -40,25 +40,24 @@ let count = 0;
 
 // This function is called every 600ms
 function update() {
-	// Play the 'tick'/'tock' sound
-    //Update the count each time arm swings
-	count++;
-    //Verify in console its counting
-	console.log(count);
-
-    playMetronome()
-    
+    if (boxMetro.checked === true){
+        playMetronome()
+    }
+    updateMetronomeCount()
 }
 
 function updateMetronomeCount() {
+    count++;
     tickCount.innerText = `Metronome Count = ${count}`;
+    if (count === 4){
+        count = 0
+    }
 }
 
 function playMetronome() {
-    updateMetronomeCount()
-	if (count === 4) {
+	if (count >= 3) {
 		tock.play();
-        count = 0;
+        // count = 0;
 	} else {
 		tick.play();
 	}
