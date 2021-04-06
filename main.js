8edbc877a632304fea39804484387954b4957cd2
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-06 11:47:27 -0400

    1b complete. added tickCounter and add the count to the DOM during the update funciton

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 // Setup 'tick' sound
-const tick = new Audio('sounds/tick.mp3');
-const tock = new Audio('sounds/tock.mp3');
+const tick = new Audio("sounds/tick.mp3");
+const tock = new Audio("sounds/tock.mp3");
+const tickCount = document.createElement("h1");
 
-let count = 0
+let count = 0;
+
+document.body.appendChild(tickCount)
+
 
 // This function is called every 600ms
 function update() {
-
-    // Play the 'tick'/'tock' sound
-    count++;
-    console.log(count)
-    if(count === 4){
-    tock.play();
-    count = 0
-    } else {
-        tick.play();
-    }
+	// Play the 'tick'/'tock' sound
+	count++;
+	console.log(count);
+	if (count === 4) {
+		tock.play();
+        tickCount.innerText = `Metronome Count = ${count}`;
+		count = 0;
+	} else {
+		tick.play();
+        tickCount.innerText = `Metronome Count = ${count}`;
+	}
 }
 
 // This function sets up update() to be called every 600ms
 function setupUpdate() {
-    setInterval(update, 600);
+	setInterval(update, 600);
 }
 
 // Call setupUpdate() once after 300ms
