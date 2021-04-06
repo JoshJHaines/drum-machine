46eb334755e70a83f2d121ae381e743c75ac0685
JoshuaJHaines <joshjhaines3@gmail.com>
2021-04-06 12:01:26 -0400

    created some comment boxes for organization and started checkbox additions

--- main.js
+++ main.js
@@ -1,24 +1,46 @@
-// Setup 'tick' sound
+/**********
+ * SOUNDS *
+ **********/
 const tick = new Audio("sounds/tick.mp3");
 const tock = new Audio("sounds/tock.mp3");
-const tickCount = document.createElement("h1");
 
-let count = 0;
+
+/*******************
+ * ADDING ELEMENTS *
+ *******************/
+const tickCount = document.createElement("h1");
+const checkBoxDiv = document.createElement("div")
 
 document.body.appendChild(tickCount)
+document.body.appendChild(checkBoxDiv)
 
+/***************
+ * OTHER SHIZZ *
+ ***************/
+let count = 0;
+
+
+/*********************************************************
+ * ****************** UPDATE FUNCTION ****************** *
+ *********************************************************/
 
 // This function is called every 600ms
 function update() {
 	// Play the 'tick'/'tock' sound
+    //Update the count each time arm swings
 	count++;
+    //Verify in console its counting
 	console.log(count);
+    //play different sound on 4th tick
 	if (count === 4) {
 		tock.play();
+        //update tickCounter with current count #
         tickCount.innerText = `Metronome Count = ${count}`;
-		count = 0;
+		//reset count to 0 after the the 4th tick
+        count = 0;
 	} else {
 		tick.play();
+        //update tickCounter with current count #
         tickCount.innerText = `Metronome Count = ${count}`;
 	}
 }
